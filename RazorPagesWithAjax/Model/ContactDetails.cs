0d45db754c549279a678fe9048f19bd1cbd3efe7
Sijmen Koffeman <koffemans@hotmail.com>
2018-04-24 14:28:14 +0100

    Added sample to git

---
+++ RazorPagesWithAjax/Model/ContactDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesWithAjax.Model
+{
+    public class ContactDetails
+    {
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ RazorPagesWithAjax/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesWithAjax.Model;
+
+namespace RazorPagesWithAjax.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public ContactDetails ContactForm { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        // handler for ajax postback
+        public async Task<IActionResult> OnPostSaveFormAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Console.WriteLine("saving to the database ...");
+            // simulate long running operation
+            Thread.Sleep(2000);
+
+            Console.WriteLine("saved to database!");
+            
+            // for example use a service to save the data
+            // await _myContactService.SaveAsync(ContactForm);
+            
+            ViewData.Add("email", ContactForm.Email);
+
+            var view = new PartialViewResult
+            {
+                // look for partial
+                ViewName = "SubmitSuccess",
+                // add the data for the partial
+                ViewData = ViewData
+            };
+
+            return view;
+        }
+    }
+}
